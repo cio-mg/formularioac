aab4273bc2c980c174612e0b9e12d473a2db4f35
Cio-mg <ciomg78@hotmail.com>
2022-06-24 16:11:47 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DocentesComponent } from './docentes/docentes.component';
+import { AdministrativosComponent } from './administrativos/administrativos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PartedosComponent } from './partedos/partedos.component';
+import { PartetresComponent } from './partetres/partetres.component';
+import { PartecuatroComponent } from './partecuatro/partecuatro.component';
+import { LoginComponent } from './app/login/login.component';
+import { RegistrarComponent } from './registrar/registrar.component';
+
+const routes: Routes = [ {path: '', component:HomeComponent, pathMatch:'full'},
+{path: 'docentes', component:DocentesComponent, pathMatch:'full'},
+{path: 'administrativos', component:AdministrativosComponent, pathMatch:'full'},
+{path: 'servicios', component:ServiciosComponent, pathMatch:'full'},
+{path: 'partedos', component:PartedosComponent, pathMatch:'full'},
+{path: 'partetres', component:PartetresComponent, pathMatch:'full'},
+{path: 'partecuatro', component:PartecuatroComponent, pathMatch:'full'},
+{path:'**', redirectTo:''}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DocentesComponent } from './docentes/docentes.component';
+import { AdministrativosComponent } from './administrativos/administrativos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PartedosComponent } from './partedos/partedos.component';
+import { PartetresComponent } from './partetres/partetres.component';
+import { PartecuatroComponent } from './partecuatro/partecuatro.component';
+import { LoginComponent } from './app/login/login.component';
+import { RegistrarComponent } from './registrar/registrar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DocentesComponent,
+    AdministrativosComponent,
+    ServiciosComponent,
+    NavbarComponent,
+    HomeComponent,
+    PartedosComponent,
+    PartetresComponent,
+    PartecuatroComponent,
+    LoginComponent,
+    RegistrarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
